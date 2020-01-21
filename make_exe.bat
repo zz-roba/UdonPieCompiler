293cffc42e40a8557e489f03800398832361ddaf
z <z>
2020-01-21 23:37:22 +0900

    update make_exe.bat, .gitignore

--- make_exe.bat
+++ make_exe.bat
@@ -1,1 +1,5 @@
 pyinstaller udon_compiler.spec
+copy /Y	 udon_funcs_data.py dist\udon_funcs_data.py
+copy /Y	 README.md dist\README.md
+xcopy /Y tools dist\
+powershell compress-archive -Force dist UdonPie_release.zip
