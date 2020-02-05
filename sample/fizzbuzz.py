93fec9fbc2378831f744c016c8b5a45fc6427186
zz_roba <unishika8@gmail.com>
2020-02-06 08:40:01 +0900

    Fix typo

--- sample/fizzbuzz.py
+++ sample/fizzbuzz.py
@@ -5,7 +5,7 @@ def _start():
   while i <= 100:
     # Logical operators ("and" and "or") cannot be written consecutively
     # ("A and B and C" must be written as "(A and B) and C").
-    if i % 3 == 0 and i % 5 == 5:
+    if i % 3 == 0 and i % 5 == 0:
       UnityEngineDebug.Log(SystemObject('fizzbuzz.'))
     elif i % 3 == 0:
       UnityEngineDebug.Log(SystemObject('fizz.'))
