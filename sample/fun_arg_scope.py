1a48540bd3db99acb5fe4452296b21b860d8e758
zz_roba <unishika8@gmail.com>
2020-02-11 12:49:34 +0900

    Update fun_arg_scope.py

--- sample/fun_arg_scope.py
+++ sample/fun_arg_scope.py
@@ -2,7 +2,18 @@ def _start():
   a = 5
   b = 7
   c = add(a, b)
-  UnityEngineDebug.Log(SystemObject("Result: " + SystemConvert.ToString(c)))
+  UnityEngineDebug.Log(SystemObject("add result: " + SystemConvert.ToString(c)))
+  UnityEngineDebug.Log(SystemObject("a = " + SystemConvert.ToString(a)))
+  UnityEngineDebug.Log(SystemObject("b = " + SystemConvert.ToString(a)))
+  var_test(a, b)
+  UnityEngineDebug.Log(SystemObject("a = " + SystemConvert.ToString(a)))
+  UnityEngineDebug.Log(SystemObject("b = " + SystemConvert.ToString(a)))
 
 def add(a: SystemInt32, b: SystemInt32) -> SystemInt32:
   return a + b
+
+def var_test(a: SystemInt32, b:SystemInt32) -> SystemVoid:
+  a = 10000
+  b = 20000
+  UnityEngineDebug.Log(SystemObject("a = " + SystemConvert.ToString(a)))
+  UnityEngineDebug.Log(SystemObject("b = " + SystemConvert.ToString(a)))
